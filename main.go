77777654936f118280440edb3381106e2d10a34e
Shawn Catanzarite <me@shawncatz.com>
2017-07-10 23:47:16 -0700

    factor out some stuff into functions

--- main.go
+++ main.go
@@ -5,46 +5,24 @@ import (
 	"os"
 	"time"
 
-	//"github.com/MAD-GooZe/jaderender"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	_ "github.com/joho/godotenv/autoload"
 
 	"github.com/dashotv/models"
+	"gopkg.in/mgo.v2/bson"
 )
 
 var tokenSecret string
 
 func main() {
 	tokenSecret = os.Getenv("TOKEN_SECRET")
-	host := os.Getenv("DATABASE_HOST")
-	name := os.Getenv("DATABASE_NAME")
 	mode := os.Getenv("GIN_MODE")
 	fmt.Printf("starting dashotv/api server (%s)...\n", mode)
 
+	corsMiddleware := configureCors()
+
 	router := gin.Default()
-	//router.HTMLRender = jaderender.Default()
-	corsConfig := cors.Config{
-		AllowOrigins: []string{"https://dasho.tv", "http://localhost:8000", "http://localhost:4200"},
-		AllowMethods: []string{"GET, POST, OPTIONS, PUT, DELETE, HEAD"},
-		AllowHeaders: []string{
-			"Origin",
-			"Content-Length",
-			"Content-Type",
-			"Authorization",
-			"if-modified-since",
-		},
-		ExposeHeaders: []string{
-			"Origin",
-			"Content-Length",
-			"Content-Type",
-			"Authorization",
-			"if-modified-since",
-		},
-		AllowCredentials: true,
-		MaxAge:           12 * time.Hour,
-	}
-	corsMiddleware := cors.New(corsConfig)
 
 	router.Use(corsMiddleware)
 	router.GET("/", homeIndex)
@@ -70,6 +48,15 @@ func main() {
 		session.DELETE("/sign_out", sessionDestroy)
 	}
 
+	initDB()
+	fmt.Println("running router")
+	router.Run()
+}
+
+func initDB() {
+	host := os.Getenv("DATABASE_HOST")
+	name := os.Getenv("DATABASE_NAME")
+
 	t1 := make(chan bool, 1)
 	go func() {
 		fmt.Printf("intializing db connection: %s/%s\n", host, name)
@@ -85,6 +72,40 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println("running router")
-	router.Run()
+	user := &models.User{}
+	err := models.DB.Users.FindOne(bson.M{"email": "shawn@dasho.net"}, user)
+	if err != nil {
+		fmt.Printf("error getting user: %s\n", err)
+		user.Email = "shawn@dasho.net"
+		user.Password = "blarg"
+		err = user.Save()
+		if err != nil {
+			fmt.Printf("error while saving user: %s\n", err)
+		}
+	}
+}
+
+func configureCors() gin.HandlerFunc {
+	corsConfig := cors.Config{
+		AllowOrigins: []string{"https://dasho.tv", "http://localhost:8000", "http://localhost:4200"},
+		AllowMethods: []string{"GET, POST, OPTIONS, PUT, DELETE, HEAD"},
+		AllowHeaders: []string{
+			"Origin",
+			"Content-Length",
+			"Content-Type",
+			"Authorization",
+			"if-modified-since",
+		},
+		ExposeHeaders: []string{
+			"Origin",
+			"Content-Length",
+			"Content-Type",
+			"Authorization",
+			"if-modified-since",
+		},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}
+
+	return cors.New(corsConfig)
 }
